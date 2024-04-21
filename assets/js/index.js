89b33d1bec7d34158f0d2f026a809d1816ee3d32
claudio <claudiosotojofre@gmail.com>
2024-04-21 01:19:36 -0400

    agregando try catch

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,40 @@
 
 const getPosts = async () => {
 
+    try {
 
-    let url = 'https://jsonplaceholder.typicode.com/posts'
+        let url = 'https://jsonplaceholder.typicode.com/posts'
 
-    let response = await fetch(url);
+        let response = await fetch(url);
 
-    let data = await response.json();
+        let data = await response.json();
 
-    console.log(data);
+        console.log(data);
 
-    let nuevoArreglo = data.map((elemento) => {
-        return `<ul>
-        <li>
-        <strong>${elemento.title}</strong>
-          <p>${elemento.body}</p>
-        </li>
-      </ul>`
-    })
+        let nuevoArreglo = data.map((elemento) => {
+            return `<ul>
+            <li>
+            <strong>${elemento.title}</strong>
+              <p>${elemento.body}</p>
+            </li>
+          </ul>`
+        })
 
-    document.getElementById("post-data").innerHTML = nuevoArreglo.join(" ");
+        document.getElementById("post-data").innerHTML = nuevoArreglo.join(" ");
+
+
+    }
+
+    catch (error) {
+
+        console.log("estamos en el catch")
+        console.log(error)
+
+
+
+    }
+
+
 
 
 
