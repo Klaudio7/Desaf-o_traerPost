dfa864fc57cc77428256976ea7331ca136d7b3a6
claudio <claudiosotojofre@gmail.com>
2024-04-21 01:05:56 -0400

    mostrando template dinamico en el dom

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,15 +10,18 @@ const getPosts = async () => {
 
     console.log(data);
 
-    let template = `<ul>
-    <li>
-    <strong>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</strong>
-      <p>quia et suscipit\nsuscipit
-        recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem
-        sunt rem eveniet architecto
-      </p>
-    </li>
-  </ul>`
+    let nuevoArreglo = data.map((elemento) => {
+        return `<ul>
+        <li>
+        <strong>${elemento.title}</strong>
+          <p>${elemento.body}</p>
+        </li>
+      </ul>`
+    })
+
+    document.getElementById("post-data").innerHTML = nuevoArreglo.join(" ");
+
+
 
 }
 
